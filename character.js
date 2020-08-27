4bc1c5f686941d75c04dfd314a56980334cbaff8
Mads Haerup <506094@rts-365.dk>
2020-08-27 14:10:38 +0200

    design og fetch

---
+++ character.js
@@ -0,0 +1,34 @@
+let url = new URLSearchParams(window.location.search);
+//console.log(url);
+if (url.has("id")) {
+    //console.log(url.get("id"));
+    fetch(`https://swapi.dev/api/people/${url.get("id")}`)
+        .then(res => res.json())
+        .then(function(data) {
+            console.log(data);
+            document.querySelector(".name").innerText = data.name
+            document.querySelector(".height").innerText = data.height
+            document.querySelector(".gender").innerText = data.gender
+            document.querySelector(".hair_color").innerText = data.hair_color
+            document.querySelector(".age").innerText = data.mass
+            document.querySelector(".birth_year").innerText = data.birth_year
+
+        });
+
+    fetch(`http://swapi.dev/api/planets/${url.get("id")}`)
+        .then(res => res.json())
+        .then(function(data) {
+            console.log(data);
+            document.querySelector(".homeworld").innerHTML = data.name
+
+        });
+
+    fetch(`http://swapi.dev/api/starships/${url.get("id")}`)
+        .then(res => res.json())
+        .then(function(data) {
+            console.log(data);
+            document.querySelector(".starship").innerHTML = data.name
+
+        });
+
+}
--- swapi.js
+++ swapi.js
@@ -4,19 +4,40 @@ fetch("https://swapi.dev/api/people")
     .then(res => res.json())
     //griber data'en og gør noget ved den
     .then(function(data) {
-        console.log(data);
+        //console.log(data);
         //refere til vores template
         let template = document.querySelector("#template");
         //referere til det sted vi ligger dataen ind i
         let starWarsList = document.querySelector("#starWarsList");
         //array'et hedder results
         data.results.forEach(function(result) {
-            console.log(result.name);
+            //console.log(result.url);
+            //splitter en string op i et array efter hvert /... split("/") = splitter efter hvert/ .. split(" ") = splitter efter hvert mellemrum
+            //split laver en string om til et array
+            let array = result.url.split("/");
+            //console.log(array);
+            //får det næst sidst element i array'et
+            let id = array[array.length - 2];
+            console.log(id);
             //kloner hvert data objekt
             let clone = template.content.cloneNode(true);
             //får fat i elementet data'en skal puttes ind i
             clone.querySelector(".listItem").innerText = result.name
-                //Tager fat i listen(starWarsList) og putter klonen ind i den
+                //linker til den nye side
+            clone.querySelector(".listItem").href = `/character-sheet.html?id=${id}`;
+
+            //Tager fat i listen(starWarsList) og putter klonen ind i den
             starWarsList.appendChild(clone);
         });
     });
+
+// ────────────────────────────────────────────────────────────────────────────────
+// vis / skjul lyssværdet 
+document.addEventListener("click", function() {
+    let plasma = document.querySelector(".plasma");
+    if (plasma.style.display === "none") {
+        plasma.style.display = "block";
+    } else {
+        plasma.style.display = "none";
+    }
+});
